a1b88dc6e4cec522520016193af8d8beb17057b1
RGibesh <ralphgibendi01@gmail.com>
2022-04-21 21:35:39 +0300

    context api

---
+++ budget-tracker/src/context/AppContext.js
@@ -0,0 +1,8 @@
+const initialState = {
+    budget: 6000,
+    expenses: [
+        { id: 12, name: 'Rent', cost: 40 },
+        { id: 13, name: 'Construction', cost: 400 },
+        { id: 14, name: 'Sacco ', cost: 50 },
+    ],
+};
